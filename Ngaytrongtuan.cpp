c98960b935acbfac407151b5e61d8a243a1230a2
namdoan9800 <64896640+namdoan9800@users.noreply.github.com>
2020-05-09 21:12:18 +0700

    Lab 3

---
+++ Ngaytrongtuan.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	printf("nhap so nguyen bat ky: ");
+	scanf("%d",&a);
+	
+	if(a==2)
+	{
+		printf("Day la thu hai");
+		
+	}else if(a==3)
+	{
+		printf("Day la thu ba");
+	}else if(a==4)
+	{
+		printf("Day la thu tu");
+	}else if (a==5)
+	{
+		printf("Day la thu nam");
+	}else if (a==6)
+	{
+		printf("Day la thu sau");
+	}else if (a==7)
+	{
+		printf("Day la thu bay");
+	}else if (a==8)
+	{
+		printf("Day la chu nhat");
+	}else
+	{
+	printf (" Day khong phai la ngay trong tuan");
+}
+}
+	
